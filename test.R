b85b5f6e54d600ef4f5570ea72c032d39fd5a59a
WilliamHR <s194545@dtu.dk>
2022-08-30 12:03:27 +0200

    Added package structure

--- test.R
+++ test.R
@@ -1,3 +1,14 @@
-print("Hello World")
-
-"first change to a new file"
+#' Title
+#'
+#' @param foo 
+#' @param bar 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+myFunction <- function(foo, bar){
+  # Do stuff with foo and bar
+  foobar <- (foo * bar) / (foo + bar)
+  return(foobar)
+}
