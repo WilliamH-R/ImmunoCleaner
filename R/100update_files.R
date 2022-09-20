e0fd6a4d73f4483df5cdb212ec787c4778caabe2
WilliamHR <s194545@dtu.dk>
2022-09-20 14:06:58 +0200

    Added import of magrittr

--- R/100update_files.R
+++ R/100update_files.R
@@ -1,8 +1,9 @@
 library("tidyverse")
+importFrom(magrittr, "%>%")
 
-update_files <- function(){
+update_files <- function() {
   list.files("data-raw", full.names = TRUE) %>%
-  stringr::str_extract(".+\\.R") %>% 
-  na.omit() %>% 
+  stringr::str_extract(".+\\.R") %>%
+  na.omit() %>%
   purrr::walk(source)
 }
