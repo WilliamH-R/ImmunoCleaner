6556bf586d2508a952cefadf50a6656326ed3202
WilliamHR <s194545@dtu.dk>
2022-09-27 00:54:25 +0200

    Added model for TCR-pairs which bind more than one pMHC

--- R/330Augment_add_non_promiscuous_TCR_pairs.R
+++ R/330Augment_add_non_promiscuous_TCR_pairs.R
@@ -4,7 +4,7 @@
 #' 
 #' @param file_name The name of the build in data file which is used.
 #'
-#' @return The same data frame, but with an added column `TCR_sequence_pair`.
+#' @return The same data frame, but with an added column `non_promiscuous_pair`.
 #'     It contains the alpha and beta chain in a pair, but only for cells
 #'     which are non-promiscuous. Meaning, where there is no doubt about
 #'     the pairing of alpha and beta sequences.
@@ -20,10 +20,11 @@ add_non_promiscuous_TCR_pairs <- function(.data,
                        dplyr::select(barcode,
                                      cell_clono_cdr3_aa),
                      by = "barcode") %>%
-    dplyr::mutate(TCR_sequence_pair = dplyr::case_when(stringr::str_count(string = cell_clono_cdr3_aa,
+    dplyr::mutate(non_promiscuous_pair = dplyr::case_when(stringr::str_count(string = cell_clono_cdr3_aa,
                                                                           pattern = "TRA") == 1 &
                                                          stringr::str_count(string = cell_clono_cdr3_aa,
                                                                             pattern = "TRB") == 1 ~ cell_clono_cdr3_aa,
-                                                       TRUE ~ NA_character_))
+                                                       TRUE ~ NA_character_)) %>% 
+    dplyr::select(-cell_clono_cdr3_aa)
   return(data_aug)
 }
--- R/430Model_significant_binder_frequency.R
+++ R/430Model_significant_binder_frequency.R
@@ -10,21 +10,30 @@
 #' @return A bar plot with the frequencies of all alleles for a particular 
 #'     TCR-sequence.
 #'
-significant_binder_frequency <- function(.data,
-                                         TCR_sequence_input = "CAARVRGFGNVLHC") {
+significant_binder_frequency <- function(.data) {
   frequency_histogram <-
     .data %>%
-    dplyr::filter(is_binder == TRUE,
-                  TCR_sequence == TCR_sequence_input) %>%
+    dplyr::filter(is_binder == TRUE) %>%
+    drop_na(non_promiscuous_pair) %>%
+    dplyr::distinct(barcode,
+                    .keep_all = TRUE) %>%
+    dplyr::group_by(non_promiscuous_pair, pMHC) %>% 
+    dplyr::count() %>%
+    dplyr::group_by(non_promiscuous_pair) %>% 
+    dplyr::mutate(n_frequency = n/sum(n)) %>% 
+    dplyr::filter(n_frequency < 1) %>% 
+    dplyr::distinct(non_promiscuous_pair)
     
-    ggplot2::ggplot(mapping = ggplot2::aes(x = pMHC,
+return(frequency_histogram)
+}
+
+
+
+'ggplot2::ggplot(mapping = ggplot2::aes(x = pMHC,
                                                     y = ..prop..,
                                                     group = 1)) +
       ggplot2::geom_bar() +
       ggplot2::coord_flip() +
       ggplot2::labs(title = "Frequencies of alleles for a specific TCR-sequence",
                     x = "Allele",
-                    y = "Frequency")
-
-return(frequency_histogram)
-}
+                    y = "Frequency")'
