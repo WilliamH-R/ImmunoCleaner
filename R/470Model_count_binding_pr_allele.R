4fa1053e9e959605c949da9601f0f771bf3185d6
WilliamHR <s194545@dtu.dk>
2022-11-14 09:43:57 +0100

    Count now indicate if HLA is matching or not

--- R/470Model_count_binding_pr_allele.R
+++ R/470Model_count_binding_pr_allele.R
@@ -22,26 +22,33 @@ count_binding_pr_allele <- function(.data,
                                     identifier = barcode) {
 
   scatter_plot <- .data %>%
-  dplyr::select({{identifier}}, pMHC, allele, is_binder) %>%
+  dplyr::select({{identifier}}, pMHC, allele, is_binder, HLA_match) %>%
   dplyr::filter(is_binder == TRUE) %>%
   dplyr::group_by({{identifier}}) %>%
   dplyr::distinct(pMHC,
                   .keep_all = TRUE) %>%
   dplyr::ungroup() %>%
-  dplyr::count(allele,
-               name = "count") %>%
 
-  ggplot2::ggplot(ggplot2::aes(x = reorder(allele, -count),
-                               y = count,
-                               label = allele)) +
-    ggplot2::geom_text(check_overlap = TRUE) +
-    ggplot2::facet_wrap(~ stringr::str_match(allele,
-                                             "[A|B|N]"),
+  ggplot2::ggplot(ggplot2::aes(x = forcats::fct_infreq(allele),
+                               fill = HLA_match)) +
+    ggplot2::geom_bar() +
+    ggplot2::facet_wrap(~stringr::str_match(allele,
+                                            "[A|B|N]"),
                         scales = "free") +
-    ggplot2::theme(axis.ticks.x = ggplot2::element_blank(),
-                   axis.text.x = ggplot2::element_blank(),
-                   axis.title = ggplot2::element_blank()) +
-    ggplot2::coord_cartesian(clip = "off")
+    ggplot2::theme(axis.title = ggplot2::element_blank()) +
+    ggplot2::scale_fill_discrete(name = "HLA-match")
 
   return(scatter_plot)
 }
+
+
+
+
+# To have one column and three rows
+#ggplot2::ggplot(ggplot2::aes(x = forcats:: fct_infreq(allele))) +
+#  ggplot2::geom_bar() +
+#  ggplot2::facet_grid(rows = dplyr::vars(stringr::str_match(allele,
+#                                                            "[A|B|N]")),
+#                      cols = dplyr::vars(donor),
+#                      scales = "free") +
+#  ggplot2::theme(axis.title = ggplot2::element_blank())
