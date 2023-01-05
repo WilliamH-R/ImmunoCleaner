c610ac9c2f6092fc6fa33f9d9ec16e994eaab102
WilliamHR <s194545@dtu.dk>
2023-01-05 08:28:00 +0100

    commented out

--- R/generate_pictures.R
+++ R/generate_pictures.R
@@ -1,33 +1,33 @@
-### Generate pictures from most functions
-# Sadly have to supply name manually
-data_combined_tidy %>%
-  alpha_beta_pair_distribution()
-
-ggplot2::ggsave("alpha_beta_pair_distribution.png",
-                path = "C:\\Users\\William\\OneDrive - Danmarks Tekniske Universitet\\Bachelor\\Pictures",
-                dpi = 300)
-
-
-
-data_combined_tidy %>%
-  dplyr::filter(is_binder == TRUE) %>%
-  alpha_beta_pair_distribution()
-
-ggplot2::ggsave("alpha_beta_pair_distribution_filter.png",
-                path = "C:\\Users\\William\\OneDrive - Danmarks Tekniske Universitet\\Bachelor\\Pictures",
-                dpi = 300)
-
-
-
-### Generate pictures from UMAP
-
-for (protein in c("CD279_PD.1")) {
-
-  data_combined_tidy %>%
-    dplyr::filter(donor == "donor1") %>%
-    umap_of_protein_expressions(color_by = protein)
-
-  ggplot2::ggsave(stringr::str_c("umap_of_protein_expressions", protein, ".png"),
-                  path = "C:\\Users\\Willi\\OneDrive - Danmarks Tekniske Universitet\\Bachelor\\Pictures",
-                  dpi = 300)
-}
+# ### Generate pictures from most functions
+# # Sadly have to supply name manually
+# data_combined_tidy %>%
+#   alpha_beta_pair_distribution()
+#
+# ggplot2::ggsave("alpha_beta_pair_distribution.png",
+#                 path = "C:\\Users\\William\\OneDrive - Danmarks Tekniske Universitet\\Bachelor\\Pictures",
+#                 dpi = 300)
+#
+#
+#
+# data_combined_tidy %>%
+#   dplyr::filter(is_binder == TRUE) %>%
+#   alpha_beta_pair_distribution()
+#
+# ggplot2::ggsave("alpha_beta_pair_distribution_filter.png",
+#                 path = "C:\\Users\\William\\OneDrive - Danmarks Tekniske Universitet\\Bachelor\\Pictures",
+#                 dpi = 300)
+#
+#
+#
+# ### Generate pictures from UMAP
+#
+# for (protein in c("CD279_PD.1")) {
+#
+#   data_combined_tidy %>%
+#     dplyr::filter(donor == "donor1") %>%
+#     umap_of_protein_expressions(color_by = protein)
+#
+#   ggplot2::ggsave(stringr::str_c("umap_of_protein_expressions", protein, ".png"),
+#                   path = "C:\\Users\\Willi\\OneDrive - Danmarks Tekniske Universitet\\Bachelor\\Pictures",
+#                   dpi = 300)
+# }
